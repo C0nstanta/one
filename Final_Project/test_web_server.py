aa710130e3f6de7a7852fe9fb0fefbbfadb299bf
admin <inter.nafta32@gmail.com>
2020-06-30 19:53:52 +0300

    test webserver

--- Final_Project/test_web_server.py
+++ Final_Project/test_web_server.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('tg', methods=['GET', 'POST'])
+@app.route('/tg', methods=['GET', 'POST'])
 def test():
     print(request)
     return ""

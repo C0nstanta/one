60ed1c3abc200a083ca4842004301cebb6a95ca0
admin <inter.nafta32@gmail.com>
2020-07-01 22:27:51 +0300

    final lesson

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -1,4 +1,6 @@
 from webshop.api import client
+from webshop.bot.main import start_bot
+from multiprocessing import Process
 
 from flask import Flask
 
@@ -6,10 +8,22 @@ from flask import Flask
 app = Flask(__name__)
 
 
-def main():
+def main_server():
     client.start_client()
 
 
+def main_client():
+    start_bot()
+
+
 if __name__ == "__main__":
-    main()
+    p1 = Process(target=main_server)
+    p1.start()
+    p2 = Process(target=main_client)
+    p2.start()
+    p1.join()
+    p2.join()
+
+    main_server()
+    main_client()
 #    app.run(debug=True)

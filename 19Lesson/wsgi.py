6ddfeb52da0c10c2b94dc9fcfae4ff07b2a3ae5d
admin <inter.nafta32@gmail.com>
2020-07-02 19:27:23 +0300

    19 lesson

--- 19Lesson/wsgi.py
+++ 19Lesson/wsgi.py
@@ -6,15 +6,15 @@ from telebot.types import Update
 
 app = Flask(__name__)
 
-@app.route(config.WEBHOOK_PATH, methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        abort(403)
+# @app.route(config.WEBHOOK_PATH, methods=['POST'])
+# def webhook():
+#     if request.headers.get('content-type') == 'application/json':
+#         json_string = request.get_data().decode('utf-8')
+#         update = Update.de_json(json_string)
+#         bot.process_new_updates([update])
+#         return ''
+#     else:
+#         abort(403)
 
 #
 # if __name__ == '__main__':
